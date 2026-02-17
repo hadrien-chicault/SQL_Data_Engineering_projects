f3a90965c8e380f5c57ef19c516b318d563cb32a
Hadrien Chicault <hadrien.chicault@pm.me>
2026-02-17 16:33:11 +0100

    DDL part 1

---
+++ Lessons/1.21/1.21_DDL_DML_pt1.sql
@@ -0,0 +1,62 @@
+-- .read ./Lessons/1.21/1.21_DDL_DML_pt1.sql
+USE data_jobs;
+DROP DATABASE IF EXISTS jobs_mart;
+CREATE DATABASE IF NOT EXISTS jobs_mart;
+-- DROP DATABASE IF EXISTS jobs_mart;
+--
+SHOW DATABASES;
+--
+-- DROP DATABASE IF EXISTS jobs_mart;
+--
+SELECT *
+FROM information_schema.schemata;
+--
+USE jobs_mart;
+--
+CREATE SCHEMA IF NOT EXISTS jobs_mart.staging;
+--DROP SCHEMA staging;
+CREATE TABLE IF NOT EXISTS staging.preferred_roles(role_id INTEGER PRIMARY KEY, role_name VARCHAR);
+--
+--DROP TABLE IF EXISTS staging.preferred_roles;
+--
+SELECT *
+FROM information_schema.tables
+where table_catalog = 'jobs_mart';
+--
+--DROP TABLE  main.preferred_roles;
+--
+INSERT INTO staging.preferred_roles (role_id, role_name)
+VALUES (1, 'Data Engineer'),
+    (2, 'Senior Data Scientist'),
+    (3, 'Software Engineer');
+--
+SELECT *
+FROM staging.preferred_roles;
+--
+ALTER TABLE IF EXISTS staging.preferred_roles
+ADD COLUMN preferred_role BOOLEAN;
+--ALTER TABLE IF EXISTS staging.preferred_roles DROP COLUMN preferred_role;
+--
+UPDATE staging.preferred_roles --
+SET preferred_role = TRUE
+WHERE role_id < 3;
+--
+UPDATE staging.preferred_roles --
+SET preferred_role = FALSE
+WHERE role_id = 3;
+--
+ALTER TABLE staging.preferred_roles
+    RENAME TO prority_roles;
+--
+ALTER TABLE staging.prority_roles
+    RENAME COLUMN preferred_role TO prority_lvl;
+--
+ALTER TABLE staging.prority_roles
+ALTER COLUMN prority_lvl TYPE INTEGER;
+--
+UPDATE staging.prority_roles
+SET prority_lvl = 3
+WHERE role_id = 3;
+--
+SELECT *
+FROM staging.prority_roles;
