468a56d07c35594473a09be6da8f2ccd73f79a2a
Hadrien Chicault <hadrien.chicault@pm.me>
2026-02-19 14:26:09 +0100

    finish lesson 1.22

--- Lessons/1.22/1.22_DDL_DML_Pt2.sql
+++ Lessons/1.22/1.22_DDL_DML_Pt2.sql
@@ -41,7 +41,7 @@ FROM staging.job_postings_flat_view
 GROUP BY job_title_short
 ORDER BY job_count DESC;
 --
-CREATE TEMPORARY TABLE senior_jobs_flat_temps AS
+CREATE TEMPORARY TABLE senior_jobs_flat_temp AS
 SELECT *
 FROM staging.job_postings_flat_view
 WHERE job_title_short = 'Senior Data Engineer';
@@ -51,3 +51,36 @@ SELECT job_title_short,
 FROM senior_jobs_flat_temps
 GROUP BY job_title_short
 ORDER BY job_count DESC;
+--
+SELECT count(*)
+FROM staging.job_postings_flat;
+SELECT count(*)
+FROM staging.job_postings_flat_view;
+SELECT count(*)
+FROM senior_jobs_flat_temp;
+--
+DELETE FROM staging.job_postings_flat
+WHERE job_posted_date >= '2024-01-01';
+--
+TRUNCATE TABLE staging.job_postings_flat;
+--
+INSERT INTO staging.job_postings_flat
+SELECT jpf.job_id,
+    jpf.job_title_short,
+    jpf.job_title,
+    jpf.job_location,
+    jpf.job_via,
+    jpf.job_schedule_type,
+    jpf.job_work_from_home,
+    jpf.search_location,
+    jpf.job_posted_date,
+    jpf.job_no_degree_mention,
+    jpf.job_health_insurance,
+    jpf.job_country,
+    jpf.salary_rate,
+    jpf.salary_year_avg,
+    jpf.salary_hour_avg,
+    cd.name AS company_name
+FROM data_jobs.job_postings_fact AS jpf
+    LEFT JOIN data_jobs.company_dim AS cd ON jpf.company_id = cd.company_id
+WHERE job_posted_date >= '2024-01-01';
