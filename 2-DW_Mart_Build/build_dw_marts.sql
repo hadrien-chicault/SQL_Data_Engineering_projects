22d362f02c169c40600177a1aab1d1a6b4b3b3ac
Hadrien Chicault <hadrien.chicault@pm.me>
2026-03-05 15:20:36 +0100

    build and valiadat flat mart

--- 2-DW_Mart_Build/build_dw_marts.sql
+++ 2-DW_Mart_Build/build_dw_marts.sql
@@ -1,5 +1,7 @@
 -- duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
 -- Step 1: DW - Create star schema tables
 .read 01_create_tables_dw.sql
--- Step 2
+-- Step 2: DW Load data from CSV file into tables
 .read 02_load_schema_dw.sql
+-- Step 3: Mart - Cret=ate flat mart
+.read 03_create_flat_mart.sql
